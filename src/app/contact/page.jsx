de30af95f56842bbe4dfb9c7fecf01c0d03f4c6d
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-02-06 22:17:57 +0600

    Contact us

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,17 @@
 export default function ContactPage() {
   return (
-    <div
-      className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat p-6"
-      style={{ backgroundImage: "" }}
-    >
-      <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-md w-full max-w-lg">
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-100 p-6 px-20">
+      <div className="w-full md:w-1/2 h-[550px]">
+        <iframe
+          className="w-full h-full rounded-lg shadow-lg"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1563.7709156056817!2d90.38997794504778!3d23.8765594647279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1738858163308!5m2!1sen!2sbd"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+
+      <div className="w-full md:w-1/2 bg-white p-8 rounded-lg shadow-lg mt-6 md:mt-0 md:ml-6">
         <h1 className="text-4xl font-bold mb-6 text-gray-800 text-center">
           Contact Us
         </h1>
@@ -45,12 +52,10 @@ export default function ContactPage() {
 
           <button
             type="submit"
-            className="w-full bg-gray-400 text-white py-3 rounded-lg hover:bg-blue-600 transition"
+            className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition"
           >
-            Send Message Send Message
-           
+            Send Message
           </button>
-       
         </form>
       </div>
     </div>
