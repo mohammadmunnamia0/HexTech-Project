e7a9386dd9b574a5243046085a17aef402839c35
RakibulHasan2 <rakibulhasan99445@gmail.com>
2025-02-06 23:14:01 +0600

    Contact page section added in Home page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/home/sections/Hero"
 import OurWorks from "./sections/OurWorks"
+import ContactPage from "../contact/page"
 
 const HomePage = () => {
     return (
         <>
             <Hero />
             <OurWorks />
+            <ContactPage />
         </>
     )
 }
