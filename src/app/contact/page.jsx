95a3042d357e76e0223d57bb53461abfc1291653
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-02-06 22:32:51 +0600

    Contact us

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,15 @@
 export default function ContactPage() {
+
+    
+    const inputClass = "w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400";
+    const labelClass = "block text-gray-700 text-sm font-bold mb-2"
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-100 p-6 px-20">
 
     {/* map */}
 
-      <div className="w-full md:w-1/2 h-[550px]">
+      <div className="w-full md:w-1/2 h-[520px]">
         <iframe
           className="w-full h-full rounded-lg shadow-lg"
           src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1563.7709156056817!2d90.38997794504778!3d23.8765594647279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1738858163308!5m2!1sen!2sbd"
@@ -23,33 +28,33 @@ export default function ContactPage() {
 
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="labelClass">
               Name
             </label>
             <input
               type="text"
               placeholder="Your Name"
-              className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"/>
+              className={inputClass}/>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="labelClass">
               Email
             </label>
             <input
               type="email"
               placeholder="Your Email"
-              className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"/>
+              className={inputClass}/>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="labelClass">
               Message
             </label>
             <textarea
               rows="4"
               placeholder="Your Message"
-              className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400">
+              className={inputClass}>
             </textarea>
           </div>
 
