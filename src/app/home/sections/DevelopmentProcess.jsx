4ce6f27a3fa18284bb569d00a50a7d3e591776ec
RakibulHasan2 <rakibulhasan99445@gmail.com>
2025-02-16 16:09:11 +0600

    fix: development process bg changed

--- src/app/home/sections/DevelopmentProcess.jsx
+++ src/app/home/sections/DevelopmentProcess.jsx
@@ -10,30 +10,54 @@ export default function DevelopmentProcess() {
         {
             title: "Initial Consultation",
             description: "We discuss your needs and objectives",
-            gif: <Image src={gif1} alt="Initial Consultation" className="h-16 w-16" />,
+            gif: (
+                <Image
+                    src={gif1}
+                    alt="Initial Consultation"
+                    className="h-16 w-16  invert brightness-0"
+                />
+            ),
         },
         {
             title: "Planning & Strategy",
             description: "Developing a tailored solution strategy",
-            gif: <Image src={gif2} alt="Initial Consultation" className="h-16 w-16" />,
+            gif: (
+                <Image
+                    src={gif2}
+                    alt="Planning & Strategy"
+                    className="h-16 w-16  invert brightness-0"
+                />
+            ),
         },
         {
             title: "Development",
             description: "Building your solution with best practices",
-       
-            gif: <Image src={gif3} alt="Initial Consultation" className="h-16 w-16" />,
+            gif: (
+                <Image
+                    src={gif3}
+                    alt="Development"
+                    className="h-16 w-16  invert brightness-0"
+                />
+            ),
         },
         {
             title: "Testing & Deployment",
             description: "Ensuring quality and smooth deployment",
-            gif: <Image src={gif4} alt="Initial Consultation" className="h-16 w-16" />,
+            gif: (
+                <Image
+                    src={gif4}
+                    alt="Testing & Deployment"
+                    className="h-16 w-16  invert brightness-0"
+                />
+            ),
         },
     ];
+
     return (
-        <section className="py-20 lg:mt-12 mt-5 lg:mb-12 mb-5">
-            <div className="container border">
-                <h2 className="text-3xl md:text-4xl font-semibold lg:mb-16 mb-5">
-                    Our Development Process
+        <section className="py-20  bg-gray-600 lg:mt-12 mt-5 lg:mb-12 mb-5">
+            <div className="container  text-white">
+                <h2 className="text-3xl lg:text-4xl font-semibold lg:mb-16 mb-5">
+                    Our Development Process -
                 </h2>
                 <div className="grid md:grid-cols-4 gap-8">
                     {developmentProcess.map((step, index) => (
