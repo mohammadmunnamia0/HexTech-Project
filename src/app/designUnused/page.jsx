3e34c59cceb85db0843b0ea8cc86f916848c769d
RakibulHasan2 <rakibulhasan99445@gmail.com>
2025-02-16 12:30:43 +0600

    feat: unused design route created with a circle development process section

---
+++ src/app/designUnused/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DevelopmentCircle from './DevelopmentCircle'
+
+export default function page() {
+  return (
+    <div className='pt-20'>
+      <DevelopmentCircle/>
+    </div>
+  )
+}
